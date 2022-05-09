905ee0c9399836b56d3b78258f5032b6ef994cd6
Tom Magorsch <tom@tomskopfbahnhof>
2022-05-09 21:41:29 +0200

    fix board test

--- tests/test_game.py
+++ tests/test_game.py
@@ -2,49 +2,49 @@
 import unittest
 import pytest
 
-from tictacslow.game import Game
+from tictacslow.board import Board
 
 
 class GameTests(unittest.TestCase):
 
     def setUp(self):
-        self.game = Game()
+        self.board = Board()
 
     def test_check_win(self):
-        self.game.make_move(2, (1,1))
-        self.game.make_move(1, (2,1))
-        self.game.make_move(2, (0,1))
-        self.game.make_move(2, (1,2))
-        self.game.make_move(1, (2,2))
-        self.game.make_move(1, (0,2))
-        self.game.make_move(1, (1,0))
-        self.game.make_move(2, (2,0))
-        self.game.make_move(1, (0,0))
-        self.assertEqual(self.game.check_win(), 3)
+        self.board.make_move(2, (1,1))
+        self.board.make_move(1, (2,1))
+        self.board.make_move(2, (0,1))
+        self.board.make_move(2, (1,2))
+        self.board.make_move(1, (2,2))
+        self.board.make_move(1, (0,2))
+        self.board.make_move(1, (1,0))
+        self.board.make_move(2, (2,0))
+        self.board.make_move(1, (0,0))
+        self.assertEqual(self.board.check_win(), 3)
 
-        self.game.reset_board()
+        self.board.reset_board()
 
-        self.game.make_move(2, (1,1))
-        self.game.make_move(2, (2,1))
-        self.game.make_move(2, (0,1))
-        self.game.make_move(2, (1,2))
-        self.game.make_move(1, (2,2))
-        self.game.make_move(1, (0,2))
-        self.game.make_move(1, (1,0))
-        self.game.make_move(2, (2,0))
-        self.game.make_move(1, (0,0))
-        self.assertEqual(self.game.check_win(), 2)
+        self.board.make_move(2, (1,1))
+        self.board.make_move(2, (2,1))
+        self.board.make_move(2, (0,1))
+        self.board.make_move(2, (1,2))
+        self.board.make_move(1, (2,2))
+        self.board.make_move(1, (0,2))
+        self.board.make_move(1, (1,0))
+        self.board.make_move(2, (2,0))
+        self.board.make_move(1, (0,0))
+        self.assertEqual(self.board.check_win(), 2)
 
-        self.game.reset_board()
+        self.board.reset_board()
 
-        self.game.make_move(1, (1,1))
-        self.game.make_move(2, (2,1))
-        self.game.make_move(2, (0,1))
-        self.game.make_move(2, (1,2))
-        self.game.make_move(1, (2,2))
-        self.game.make_move(1, (0,2))
-        self.game.make_move(1, (1,0))
-        self.game.make_move(2, (2,0))
-        self.game.make_move(1, (0,0))
-        self.game.print_board()
-        self.assertEqual(self.game.check_win(), 1)
+        self.board.make_move(1, (1,1))
+        self.board.make_move(2, (2,1))
+        self.board.make_move(2, (0,1))
+        self.board.make_move(2, (1,2))
+        self.board.make_move(1, (2,2))
+        self.board.make_move(1, (0,2))
+        self.board.make_move(1, (1,0))
+        self.board.make_move(2, (2,0))
+        self.board.make_move(1, (0,0))
+        self.board.print_board()
+        self.assertEqual(self.board.check_win(), 1)
